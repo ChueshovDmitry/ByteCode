a76c2d799661641cf8a552ba935818a202cc5f86
Dmitry Chueshov <dimka-chueshov@mail.ru>
2021-08-25 23:18:16 +0300

    sss

---
+++ src/com/company/Hello.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * @author Dmitry Chueshov 25.08.2021 23:16
+ * @project Shk
+ */
+
+public class Hello {
+    public Hello() {
+        System.out.println("hello");
+    }
+}
