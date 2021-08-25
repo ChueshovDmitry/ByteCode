df7355bc01fce6f4c82706097c08a93a787dbe0e
Dmitry Chueshov <dimka-chueshov@mail.ru>
2021-08-26 00:06:06 +0300

    Revert "add onem"
    This reverts commit 732dd6c3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,6 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        
-        //break(goto) and return test byte code
-        TestSwitch testSwitch = new TestSwitch();
-        testSwitch.setName("s");
-        testSwitch.a();
-        System.out.println(testSwitch.getName());
     
-        TestException testException = new TestException();
-        testException.creteTestTryCath();
-        ssss sssss
-        S.ss();
     }
 }
--- src/com/company/Two.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/**
- * @author Dmitry Chueshov 25.08.2021 22:19
- * @project Shk
- */
-
-public class Two {
-}
