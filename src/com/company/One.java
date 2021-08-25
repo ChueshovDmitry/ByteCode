11143126d6a0fb70cc8aece7fd72c9a09bcbf05e
Dmitry Chueshov <dimka-chueshov@mail.ru>
2021-08-26 00:02:02 +0300

    Revert "add onem"
    This reverts commit 732dd6c3

---
+++ src/com/company/One.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * @author Dmitry Chueshov 25.08.2021 21:53
+ * @project Shk
+ */
+
+public class One {
+    public One() {
+    }
+}
--- src/com/company/Two.java
+++ src/com/company/Two.java
@@ -5,5 +5,5 @@ package com.company;
  * @project Shk
  */
 
-public class wo {
+public class Two {
 }
